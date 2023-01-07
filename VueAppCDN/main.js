cb2350fae9cbd2b439791b36cbb1d6c7198b3475
Himanshu Patel <himanshu.patel.main@gmail.com>
2023-01-07 12:13:24 +0530

    event handling

--- VueAppCDN/main.js
+++ VueAppCDN/main.js
@@ -11,11 +11,39 @@ const app = Vue.createApp({
                 {id:0001, size: 'large'},
                 {id:0002, size: 'small'}
             ],
+
+            fruits: [
+                {id: 0101, name: 'apple', qty:50, image: "./res/apple.jpg", color: "red"},
+                {id: 0102, name: 'orange', qty: 20, image: "./res/orange.jpg", color: "orange"}
+            ],
             
             onSale: true,
-            qty: 7,
+            qty: 50,
             repo: "https://github.com/emeraldahp/vueJs",
+            cart: 0
         }
+    },
+    methods: {
+        addToCart(fruitName) {
+            this.cart+=1;
+        },
+        removeFromCart(fruitName) {
+            if(this.cart>0)
+                this.cart-=1;
+        },
+        selectItem(fruitName){
+            this.fruits.forEach(fruit => {
+                if(fruit.name == fruitName)
+                {
+                    this.item = fruit.name;
+                    this.image = fruit.image;
+                    this.infos[1] = fruit.color;
+                    this.qty = fruit.qty;
+                }
+            });
+        }
+
+
     }
 })
 
